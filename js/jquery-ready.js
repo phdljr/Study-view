b868c76e30392fb11ab9027306e65d15b8442bbf
phdljr <phdljr@gmail.com>
2023-03-09 16:38:38 +0900

    add: CSS position/display

---
+++ js/jquery-ready.js
@@ -0,0 +1,10 @@
+$(document).ready(()=>{
+    const test = document.querySelector("body");
+    console.log(test.childNodes)
+})
+
+const test = [];
+test.push(1);
+console.log(test);
+
+test = [1, 2, 3];
---
+++ js/study-promise.js
@@ -0,0 +1,58 @@
+// $(document).ready(()=>{
+        //     loginCheck()
+        // });
+
+        // function loginCheck() {
+        //     // resolve => 성공 / reject : 실패
+        //     return new Promise((resolve, reject)=>{
+        //         console.log(1);
+        //         $.ajax({
+        //             asdfasdf,
+        //             success: (result)=> {
+        //                 roleCheck();
+        //             },
+        //             error: ()=> {
+        //                 reject();
+        //             }
+        //         })
+        //     });
+        // }
+
+        // function roleCheck() {
+        //     // resolve => 성공 / reject : 실패
+        //     return new Promise((resolve, reject)=>{
+        //         console.log(1);
+        //         $.ajax({
+        //             asdfasdf,
+        //             success: (result)=> {
+        //                 resolve()
+        //             },
+        //             error: ()=> {
+        //                 reject();
+        //             }
+        //         })
+        //     });
+        // }
+
+        // function loginCheck() {
+        //     // resolve => 성공 / reject : 실패
+        //     return new Promise((resolve, reject)=>{
+        //         console.log(1);
+        //         $.ajax({
+        //             asdfasdf,
+        //             success: (result)=> {
+        //                 resolve()
+        //             },
+        //             error: ()=> {
+        //                 reject();
+        //             }
+        //         })
+        //     });
+        // }
+
+
+        /**
+            1. ID/PW 검증(ajax)
+            2. 권한 (ajax)
+            3. 어느페이지로 이동 (window.location)
+        */
